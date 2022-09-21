c46ce5e9ec6cce024d18f6d24902638411f81407
bigseung12 <bigseung12@gmail.com>
2022-09-21 20:02:59 +0900

    2022.09.21 과제 구현 중

--- Study/Assets/Scripts/PlayerMove.cs
+++ Study/Assets/Scripts/PlayerMove.cs
@@ -21,11 +21,25 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow)) { _inputX += 1; }
 
         Move();
+        DrawLine();
     }
 
     private void Move()
     {
-        Vector3 movePosition = MoveSpeed * Time.deltaTime * new Vector3(_inputX, 0f, _inputY).normalized;
+        Vector3 movePosition = new Vector3(_inputX, _inputY, 0f).normalized * MoveSpeed * Time.deltaTime;
         transform.position += movePosition;
+    }
+
+    private void DrawLine()
+    {
+        Vector3 upPosition = new Vector3(0f, 0.5f, 0f);
+        Vector3 downPosition = new Vector3(0f, -0.5f, 0f);
+        Vector3 leftPosition = new Vector3(-0.5f, 0f, 0f);
+        Vector3 rightPosition = new Vector3(0.5f, 0f, 0f);
+
+        Debug.DrawLine(transform.position, upPosition);
+        Debug.DrawLine(transform.position, downPosition);
+        Debug.DrawLine(transform.position, leftPosition);
+        Debug.DrawLine(transform.position, rightPosition);
     }
 }
